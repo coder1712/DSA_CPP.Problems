454383632a8cc5bc4d4767304a7494668281ef4a
Anshul Rana <anshulranacoc2002@gmail.com>
2021-12-16 23:20:24 +0530

    Create Basic.cpp

---
+++ Basic.cpp
@@ -0,0 +1,29 @@
+//Point reflection
+//given x and y; z = 2y-x; reflection against y
+//Pigeonhole Principle: If there are three pigeons and two nests,
+//then there will always be two pigeons in one nest
+//Minimum no. of draws to find a pair of socks from n pair of socks:n-1
+//Handshakes in a room with n people = n*(n-1)/2
+//No. of digits in a factorial of a number: use: log(a*b) = log(a)+log(b);
+// Natural numbers having three divisor are perfect prime square.e.g 4,9,25 (2,3,5)squares of prime numbers.
+/*Imp. Mathematics Topics:-
+1:No. of digits in a number(N): floor(log10(N)+1) T.C= O(1);
+2:Arithmetic Progression:
+    nth term:- a+(n-1)d;
+    Sum of ‘n’ terms of an AP =  (n/2)*(first term + last term) = 0.5*n*[2a + (n-1)d]
+3:Geometric Progression:
+    nth term of a GP = a*rn-1.
+    Geometric Mean = nth root of the product of n terms in the GP.
+    Sum of ‘n’ terms of a GP (r < 1) = [a (1 – rn)] / [1 – r].
+    Sum of ‘n’ terms of a GP (r > 1) = [a (rn – 1)] / [r – 1].
+    Sum of infinite terms of a GP (r < 1) = (a) / (1 – r).
+4:Quadratic Equation:
+    roots = (-b ± √(b2 - 4ac))/2a
+    If b2 < 4ac, then roots are complex
+    If b2 > 4ac, then roots are real and different
+    If b2 = 4ac, then roots are real and both roots are same.
+5:Prime Numbers: Sieve of Eratosthenes :generate prime numbers up to a given number N
+6:Factorials
+7:Permutation And Combination
+8: Modular Arithmetic.
+
